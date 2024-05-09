62229eaccf0ba1613f6b34baf32a1ebeed2b69b2
connbrack <connor.brackley@gmail.com>
2024-05-08 20:24:27 -0400

    render test 4

--- front/src/components/EnablePush.js
+++ front/src/components/EnablePush.js
@@ -18,27 +18,44 @@ const EnablePush = () => {
           console.log("Subscription found");
           console.log(subscription);
           sendSubscriptionToServer(subscription);
-
         } else {
           console.log("No subscription");
+          subscribeUser(registration);
         }
       });
     });
   }
+  
+  function subscribeUser(registration) {
+    const applicationServerKey = urlBase64ToUint8Array("BFKuvwD0KjlRgrryYFo58CqLJ-bg-z3qGNh3Qxw5T6A_4kSQsgCNoH7mH03V2dx4E8Hlrwe-ZsXlUpnoacakeuY");
+    registration.pushManager
+      .subscribe({
+        userVisibleOnly: true,
+        applicationServerKey: applicationServerKey,
+      })
+      .then((subscription) => {
+        console.log("User is subscribed.");
+        sendSubscriptionToServer(subscription);
+      })
+      .catch((err) => {
+        console.error("Failed to subscribe the user: ", err);
+      });
+  }
 
   function sendSubscriptionToServer(subscription) {
-      fetch('/api/send-test-message', {
-          method: 'post',
-          headers: {
-              'Content-Type': 'application/json',
-          },
-          body: JSON.stringify(subscription)
-      }).then(response => response.json())
-      .catch(error => console.error('Error sending subscription:', error));
+    fetch("/api/send-test-message", {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(subscription),
+    })
+      .then((response) => response.json())
+      .catch((error) => console.error("Error sending subscription:", error));
   }
 
   function testPush() {
-    checkAndResubscribe()
+    checkAndResubscribe();
   }
 
   return (
