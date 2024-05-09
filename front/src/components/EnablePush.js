334ece29a94a9bb34eb2cc07b9a0973b930be575
connbrack <connor.brackley@gmail.com>
2024-05-08 20:31:09 -0400

    render test 3

--- front/src/components/EnablePush.js
+++ front/src/components/EnablePush.js
@@ -25,9 +25,27 @@ const EnablePush = () => {
       });
     });
   }
-  
+
+  const urlBase64ToUint8Array = (base64String) => {
+    const padding = "=".repeat((4 - (base64String.length % 4)) % 4);
+    const base64 = (base64String + padding)
+      .replace(/\-/g, "+")
+      .replace(/_/g, "/");
+
+    const rawData = atob(base64);
+    const outputArray = new Uint8Array(rawData.length);
+
+    for (let i = 0; i < rawData.length; ++i) {
+      outputArray[i] = rawData.charCodeAt(i);
+    }
+
+    return outputArray;
+  };
+
   function subscribeUser(registration) {
-    const applicationServerKey = urlBase64ToUint8Array("BFKuvwD0KjlRgrryYFo58CqLJ-bg-z3qGNh3Qxw5T6A_4kSQsgCNoH7mH03V2dx4E8Hlrwe-ZsXlUpnoacakeuY");
+    const applicationServerKey = urlBase64ToUint8Array(
+      "BFKuvwD0KjlRgrryYFo58CqLJ-bg-z3qGNh3Qxw5T6A_4kSQsgCNoH7mH03V2dx4E8Hlrwe-ZsXlUpnoacakeuY"
+    );
     registration.pushManager
       .subscribe({
         userVisibleOnly: true,
